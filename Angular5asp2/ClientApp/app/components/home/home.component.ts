cd7cb9ffbd0de388b97a9db9250d9a477e90b111
vnntptt <vo_ngoc_nhan@yahoo.com>
2018-03-23 00:29:01 +0700

    2

--- Angular5asp2/ClientApp/app/components/home/home.component.ts
+++ Angular5asp2/ClientApp/app/components/home/home.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
-
 @Component({
     selector: 'home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    styleUrls: [
+        '../../../assets/global/plugins/uniform/css/uniform.default.css',
+        '../../../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css',
+
+        '../../../assets/global/plugins/select2/css/select2.min.css',
+        '../../../assets/global/plugins/select2/css/select2-bootstrap.min.css',
+
+        '../../../assets/global/css/components.min.css',
+        '../../../assets/global/css/plugins.min.css',
+        '../../../assets/pages/css/login-5.min.css',
+    ]
+
 })
 export class HomeComponent {
 }
